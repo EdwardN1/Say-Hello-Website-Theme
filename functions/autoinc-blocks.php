c433fd9bd87b6c9b8225d2086ed2cb2bd7f9f944
EdwardN1 <edward@technicks.com>
2019-01-02 12:25:36 +0000

    Initial Commit

---
+++ functions/autoinc-blocks.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Edward Nickerson
+ * Date: 16/12/2018
+ * Time: 06:41
+ */
+
+add_filter( 'block_categories', function( $categories, $post ) {
+	return array_merge(
+		$categories,
+		array(
+			array(
+				'slug' => 'sayhelloblocks',
+				'title' => __( 'Say Hello Blocks', 'sayhelloblocks' ),
+			),
+		)
+	);
+}, 10, 2 );
+
+
+// Update CSS within in Admin
+function admin_style() {
+	$version=filemtime(get_template_directory().'/assets/styles/style.css');
+	wp_enqueue_style('admin-styles', get_template_directory_uri().'/assets/styles/admin.css?v='.$version);
+}
+add_action('admin_enqueue_scripts', 'admin_style');
